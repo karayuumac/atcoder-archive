0f9c6fd3126e3c472b664ecc858b8c030aa3fd9d
karayuu <karayan0614@gmail.com>
2018-12-08 21:11:33 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by karayuu on 2018/12/08
+ */
+fun main(args: Array<String>) {
+    val d = readLine()!!.toInt()
+    when (d) {
+        25 -> println("Christmas")
+        24 -> println("Christmas Eve")
+        23 -> println("Christmas Eve Eve")
+        22 -> println("Christmas Eve Eve Eve")
+    }
+}
