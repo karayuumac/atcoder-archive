ab550c22dbe9b68ccdb03766183275c2d30d57ca
karayuu <karayan0614@gmail.com>
2019-01-20 21:09:13 +0900

    [AC] abc116 abc116_a

---
+++ atcoder.jp/abc116/abc116_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <cmath>
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define FORN(i, a, b) for (int i = a; i <= b; i++)
+
+using namespace std;
+//
+// Created by karayuu on 2019-01-20.
+//
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a * b / 2 << endl;
+}
