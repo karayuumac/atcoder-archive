ef57fda9da466f437a14008d973a40ace8271a75
karayuu <karayan0614@gmail.com>
2019-02-16 22:14:54 +0900

    [AC] abc118 abc118_a

---
+++ atcoder.jp/abc118/abc118_a/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * @author karayuu
+ */
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    if (b % a == 0) {
+        println(a + b)
+    } else {
+        println(b - a)
+    }
+}
