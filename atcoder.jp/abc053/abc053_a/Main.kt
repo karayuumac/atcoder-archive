0a193749ce0ee799a4fbade547e8465fdcaef4d1
karayuu <karayan0614@gmail.com>
2019-06-17 13:17:26 +0900

    [AC] abc053 abc053_a

---
+++ atcoder.jp/abc053/abc053_a/Main.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by karayuu on 2019/06/17
+ */
+fun main(args: Array<String>) {
+    val x = readLine()!!.toInt()
+    if (x < 1200) {
+        println("ABC")
+    } else {
+        println("ARC")
+    }
+}
