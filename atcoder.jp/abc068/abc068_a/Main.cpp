05b9ff0a3f5034da8658b1778a6e44ce84bcd935
karayuu <karayan0614@gmail.com>
2019-08-03 15:56:07 +0900

    [AC] abc068 abc068_a

---
+++ atcoder.jp/abc068/abc068_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << "ABC" << n << endl;
+}
