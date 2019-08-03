e39de15ab8b0af8724746baaf75c559758273cf2
karayuu <karayan0614@gmail.com>
2019-08-03 17:27:13 +0900

    [AC] abc069 abc069_a

---
+++ atcoder.jp/abc069/abc069_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  cout << (n - 1) * (m - 1) << endl;
+}
