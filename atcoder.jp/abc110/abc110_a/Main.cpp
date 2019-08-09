29c453b72547e1adbc71cc1636136fb68248a95c
karayuu <karayan0614@gmail.com>
2019-08-10 02:29:38 +0900

    [AC] abc110 abc110_a

---
+++ atcoder.jp/abc110/abc110_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+int main() {
+  vector<int> v;
+  load(v, 3);
+  sort(v);
+
+  cout << v[2] * 10 + v[1] + v[0] << endl;
+}
