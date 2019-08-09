85c5d104e813ebea45f39b733b8e2167c38f67f9
karayuu <karayan0614@gmail.com>
2019-08-10 02:57:49 +0900

    [AC] abc125 abc125_a

---
+++ atcoder.jp/abc125/abc125_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+int main() {
+  int a, b, t;
+  cin >> a >> b >> t;
+
+  cout << b * (int) ((t + 0.5) / a) << endl;
+}
