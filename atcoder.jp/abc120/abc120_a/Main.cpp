83f8c3824c68258ba7cfead5fc0e841b9f20e6c9
karayuu <karayan0614@gmail.com>
2019-08-10 02:43:19 +0900

    [AC] abc120 abc120_a

---
+++ atcoder.jp/abc120/abc120_a/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << min(b / a, c) << endl;
+}
