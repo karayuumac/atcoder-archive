bd0acc61ef705f3e32519bf85e9de47bc54bae29
karayuu <karayan0614@gmail.com>
2019-08-10 02:24:54 +0900

    [AC] abc108 abc108_a

---
+++ atcoder.jp/abc108/abc108_a/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+int main() {
+  int k;
+  cin >> k;
+
+  cout << (k / 2) * (k - k / 2) << endl;
+}
