79ad2627d2d78b88d4fabcb45abce11b2e8bcb24
karayuu <karayan0614@gmail.com>
2019-08-10 02:37:01 +0900

    [AC] abc114 abc114_a

---
+++ atcoder.jp/abc114/abc114_a/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to));
+#define sort(a) sort(a.begin(), a.end())
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+int main() {
+  int x;
+  cin >> x;
+  if (x == 3 || x == 5 || x == 7) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
