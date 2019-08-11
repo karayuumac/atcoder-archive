cece21ac06484bd3532de1b259cb6f7a84a121b9
karayuu <karayan0614@gmail.com>
2019-08-11 13:11:37 +0900

    [AC] ddcc2017-qual ddcc2017_qual_a

---
+++ atcoder.jp/ddcc2017-qual/ddcc2017_qual_a/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+//#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  
+  if (s[0] == s[1] && s[1] != s[2] && s[2] == s[3]) {
+    p("Yes");
+  } else {
+    p("No");
+  }
+}
