81ecf47449b89af70b7dcb7f8b0a0f30cd3189e8
karayuu <karayan0614@gmail.com>
2019-08-14 00:34:54 +0900

    [AC] abc127 abc127_c

---
+++ atcoder.jp/abc127/abc127_c/Main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  int mi = 0, ma = n;
+  rep(i, m) {
+    int l, r;
+    cin >> l >> r;
+
+    mi = max(mi, l);
+    ma = min(ma, r);
+  }
+
+  //max - min + 1だけではダメで、max < minの場合は0であることに注意する
+  if (mi <= ma) {
+    p(ma - mi + 1);
+  } else {
+    p(0);
+  }
+}
