f7b59c089d1c961c3a87fcdd8cf05f941f047e83
karayuu <karayan0614@gmail.com>
2019-08-18 03:39:12 +0900

    [AC] abc132 abc132_b

---
+++ atcoder.jp/abc132/abc132_b/Main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> v;
+  load(v, n);
+
+  int c = 0;
+  for (int i = 0; i < n - 2; ++i) {
+    if ((v[i] < v[i + 1] && v[i + 1] < v[i + 2])
+        || (v[i] > v[i + 1] && v[i + 1] > v[i + 2])) {
+      c++;
+    }
+  }
+  p(c);
+}
