62fd146b25e8fd7f69e218f5ff479fb952cc0353
karayuu <karayan0614@gmail.com>
2019-08-19 08:43:31 +0900

    [AC] abc106 abc106_b

---
+++ atcoder.jp/abc106/abc106_b/Main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  int count = 0;
+  for (int i = 1; i <= n; i += 2) {
+    int c = 0;
+    for (int j = 1; j <= i; ++j) {
+      if (i % j == 0) {
+        c++;
+      }
+    }
+    if (c == 8) {
+      count++;
+    }
+  }
+  p(count);
+}
