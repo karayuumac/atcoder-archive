2cb78bdf444c044351f818a345a0a672967edbea
karayuu <karayan0614@gmail.com>
2019-08-19 02:37:25 +0900

    [AC] agc025 agc025_a

---
+++ atcoder.jp/agc025/agc025_a/Main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  int mi = 1e9;
+  for (int i = 1; i <= n / 2; ++i) {
+    int a = i;
+    int b = n - i;
+
+    string sa, sb;
+    sa = to_string(a);
+    sb = to_string(b);
+    int sum = 0;
+    for (char j : sa) {
+      sum += (j - '0');
+    }
+    for (char j : sb) {
+      sum += (j - '0');
+    }
+    mi = min(sum, mi);
+  }
+  p(mi);
+}
