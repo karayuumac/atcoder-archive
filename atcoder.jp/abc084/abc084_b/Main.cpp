9b9f48a909fd085121d0b274dd7e5dc10ae0116a
karayuu <karayan0614@gmail.com>
2019-08-21 07:17:36 +0900

    [AC] abc084 abc084_b

---
+++ atcoder.jp/abc084/abc084_b/Main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int a, b; string s;
+  cin >> a >> b >> s;
+
+  bool c = false;
+  for (int i = 0; i < a + b + 1; ++i) {
+    if (i == a && s[i] == '-') {
+      c = true;
+      continue;
+    } else if ('0' <= s[i] && s[i] <= '9') {
+      continue;
+    } else {
+      pn();
+      return 0;
+    }
+  }
+
+  if (!c) {
+    pn();
+  } else {
+    py();
+  }
+}
