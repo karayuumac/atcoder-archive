482ce333ed571fc84a731965bbb65dcaeab6b1dd
karayuu <karayan0614@gmail.com>
2019-08-22 15:31:17 +0900

    [AC] abc104 abc104_b

---
+++ atcoder.jp/abc104/abc104_b/Main.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+
+  int c = 0, index = 0;
+  for (int i = 0; i < s.length(); ++i) {
+    if (i == 0) {
+      if (s[i] != 'A') {
+        p("WA");
+        return 0;
+      }
+      continue;
+    } else if (s[i] == 'C') {
+      index = i;
+      c++;
+    } else if (!('a' <= s[i] && s[i] <= 'z')) {
+      p("WA");
+      return 0;
+    }
+  }
+  if (c != 1 || !(2 <= index && index <= s.length() - 2)) {
+    p("WA");
+    return 0;
+  }
+  p("AC");
+}
