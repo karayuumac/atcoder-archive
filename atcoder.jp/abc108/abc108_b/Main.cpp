5cf5b9279e80b54fafcf39b1530bd85a3d553703
karayuu <karayan0614@gmail.com>
2019-08-23 21:14:16 +0900

    [AC] abc108 abc108_b

---
+++ atcoder.jp/abc108/abc108_b/Main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int x1, y1, x2, y2;
+  cin >> x1 >> y1 >> x2 >> y2;
+
+  int x3 = x2 + y1 - y2;
+  int y3 = y2 + x2 - x1;
+
+  int x4 = x3 - (x2 - x1);
+  int y4 = y3 - (y2 - y1);
+
+  cout << x3 << " " << y3 << " " << x4 << " " << y4;
+}
