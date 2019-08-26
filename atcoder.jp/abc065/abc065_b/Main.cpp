16062ffcc4ea8c2b6ad621ad35a63ed60d5f31be
karayuu <karayan0614@gmail.com>
2019-08-26 14:17:55 +0900

    [AC] abc065 abc065_b

---
+++ atcoder.jp/abc065/abc065_b/Main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define sort(a) sort(a.begin(), a.end())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int a[n + 1];
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+  }
+
+  int index = 1;
+  int count = 0;
+
+  while (index != 2) {
+    if (count == n) {
+      p(-1);
+      return 0;
+    }
+    index = a[index];
+    count++;
+  }
+
+  p(count);
+}
