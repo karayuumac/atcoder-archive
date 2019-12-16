c62537f85c7ba0eaf94c7423f9b44b92a2a9c4a1
karayuu <karayan0614@gmail.com>
2019-12-17 04:59:31 +0900

    [AC] abc051 abc051_b

---
+++ atcoder.jp/abc051/abc051_b/Main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define _sort(a) sort(a.begin(), a.end())
+#define _sortd(a) sort(a.rbegin(), a.rend())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  int k, s;
+  cin >> k >> s;
+
+  int c = 0;
+  for (int i = 0; i <= k; ++i) {
+    for (int j = 0; j <= k; ++j) {
+      if (0 <= s - i - j && s - i - j <= k) {
+        c++;
+      }
+    }
+  }
+  p(c);
+}
