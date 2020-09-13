400e3c63c3dd5b4298197e9643167bd1d2b2f752
karayuu <karayan0614@gmail.com>
2020-09-13 21:28:32 +0900

    [AC] abc178 abc178_c

---
+++ atcoder.jp/abc178/abc178_c/Main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define _sort(a) sort(a.begin(), a.end())
+#define _sortd(a) sort(a.rbegin(), a.rend())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+ll calc(int x, int y) {
+  ll result = 1;
+  for (int i = 1; i <= y; i++) {
+    result *= x;
+    result = result % MOD;
+  }
+  return result;
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  ll result = ((calc(10, n) - calc(9, n)) * 2 - (calc(10, n) - calc(8, n))) % MOD;
+  if (result < 0) {
+    result = MOD + result;
+  }
+  cout << result << endl;
+}
