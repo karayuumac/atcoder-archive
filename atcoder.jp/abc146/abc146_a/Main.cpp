b47c29d17665528f23a90cf829367dc4166743f8
karayuu <karayan0614@gmail.com>
2020-10-05 14:00:42 +0900

    [AC] abc146 abc146_a

---
+++ atcoder.jp/abc146/abc146_a/Main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define _sort(a) sort(a.begin(), a.end())
+#define _sortd(a) sort(a.rbegin(), a.rend())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  map<string, int> map = {
+          {"SUN", 7},
+          {"MON", 6},
+          {"TUE", 5},
+          {"WED", 4},
+          {"THU", 3},
+          {"FRI", 2},
+          {"SAT", 1},
+  };
+  cout << map[s] << endl;
+}
