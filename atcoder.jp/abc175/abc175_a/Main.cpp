f2650a61a35f05069af02a0914b00591d6537b6a
karayuu <karayan0614@gmail.com>
2020-10-07 17:07:39 +0900

    [AC] abc175 abc175_a

---
+++ atcoder.jp/abc175/abc175_a/Main.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+const int MOD = 1000000007;
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define copy(from, to) copy(from.begin(), from.end(), back_inserter(to))
+#define _sort(a) sort(a.begin(), a.end())
+#define _sortd(a) sort(a.rbegin(), a.rend())
+#define reverse(s) reverse(s.begin(), s.end())
+#define p(s) cout << (s) << endl
+
+typedef long long ll;
+using namespace std;
+
+template<class T>
+void load(vector<T> &v, int n) {
+  for (int i = 0; i < n; ++i) {
+    T t;
+    cin >> t;
+    v.push_back(t);
+  }
+}
+
+template<class T>
+T max(vector<T> &v) {
+  return *max_element(v.begin(), v.end());
+}
+
+template<class T>
+T min(vector<T> &v) {
+  return *min_element(v.begin(), v.end());
+}
+
+void py(bool isUpper = false) {
+  if (isUpper) {
+    p("YES");
+  } else {
+    p("Yes");
+  }
+}
+
+void pn(bool isUpper = false) {
+  if (isUpper) {
+    p("NO");
+  } else {
+    p("No");
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  if (s == "RRR") {
+    cout << 3 << endl;
+    return 0;
+  }
+  if (s == "RRS" || s == "SRR") {
+    cout << 2 << endl;
+    return 0;
+  }
+  if (s == "RSR" || s == "RSS" || s == "SSR" || s == "SRS") {
+    cout << 1 << endl;
+    return 0;
+  } else {
+    cout << 0 << endl;
+  }
+}
