30100f290e038c7e7c97b462a29bce33929d39fe
karayuu <karayan0614@gmail.com>
2021-02-23 12:26:40 +0900

    [AC] abc154 abc154_b

---
+++ atcoder.jp/abc154/abc154_b/Main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define repd(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+
+#define FOR(i, a, b) for(ll i = a; i <= ll(b); i++)
+
+
+int main() {
+  string s;
+  cin >> s;
+
+  rep(i, s.size()) {
+    cout << "x";
+  }
+  cout << endl;
+}
