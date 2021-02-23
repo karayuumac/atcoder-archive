1577879a22c2ebf905e6689745aa4bb186b89bf9
karayuu <karayan0614@gmail.com>
2021-02-23 11:54:12 +0900

    [AC] abl abl_a

---
+++ atcoder.jp/abl/abl_a/Main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define repd(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+
+#define FOR(i, a, b) for(ll i = a; i <= ll(b); i++)
+
+
+int main() {
+  int k;
+  cin >> k;
+  rep(i, k) {
+    cout << "ACL";
+  }
+  cout << endl;
+}
