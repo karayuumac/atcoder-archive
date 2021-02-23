933cbfa9ae60edb7a5c898d1595e37c8b864036f
karayuu <karayan0614@gmail.com>
2021-02-23 12:21:29 +0900

    [AC] tokiomarine2020 tokiomarine2020_a

---
+++ atcoder.jp/tokiomarine2020/tokiomarine2020_a/Main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define repd(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+
+#define FOR(i, a, b) for(ll i = a; i <= ll(b); i++)
+
+
+int main() {
+  string s;
+  cin >> s;
+  cout << s[0] << s[1] << s[2] << endl;
+}
