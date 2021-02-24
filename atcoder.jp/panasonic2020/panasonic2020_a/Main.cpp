f55dc259af8b60f7fc003f62527ee3fce65cc3dd
karayuu <karayan0614@gmail.com>
2021-02-24 15:54:54 +0900

    [AC] panasonic2020 panasonic2020_a

---
+++ atcoder.jp/panasonic2020/panasonic2020_a/Main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define repd(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+
+#define FOR(i, a, b) for(ll i = a; i <= ll(b); i++)
+
+
+int main() {
+  vector<int> s = { 1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51 };
+  int k;
+  cin >> k;
+  
+  cout << s[k - 1] << endl;
+}
