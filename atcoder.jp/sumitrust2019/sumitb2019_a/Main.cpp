242b1204b11d52e5f3dec093a87e63f5bd83c333
karayuu <karayan0614@gmail.com>
2021-02-24 22:07:52 +0900

    [AC] sumitrust2019 sumitb2019_a

---
+++ atcoder.jp/sumitrust2019/sumitb2019_a/Main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define repd(i, n) for(ll i = ll(n - 1); i >= 0; i--)
+
+#define FOR(i, a, b) for(ll i = a; i <= ll(b); i++)
+
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  
+  if (d == 1) {
+    cout << "1" << endl;
+  } else {
+    cout << "0" << endl;
+  }
+}
