57e684fb65ea988cf1a10f1f67ed5f9b57a5c81b
karayuu <karayan0614@gmail.com>
2021-03-07 11:41:31 +0900

    [AC] abc159 abc159_c

---
+++ atcoder.jp/abc159/abc159_c/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template <typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  double l;
+  cin >> l;
+
+  cout << printf("%f", pow(l / 3, 3)) << endl;
+}
