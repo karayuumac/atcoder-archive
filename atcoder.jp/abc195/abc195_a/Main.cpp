4ea8b5a57b5046b4e5e8cb38ded42b78ea8694cd
karayuu <karayan0614@gmail.com>
2021-03-13 21:00:58 +0900

    [AC] abc195 abc195_a

---
+++ atcoder.jp/abc195/abc195_a/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  int m, h;
+  cin >> m >> h;
+  if (h % m == 0) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
