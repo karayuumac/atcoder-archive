29f3c331eaeb6b5ba691c097a96100d5d1661b20
karayuu <karayan0614@gmail.com>
2021-03-18 20:15:29 +0900

    [AC] agc020 agc020_a

---
+++ atcoder.jp/agc020/agc020_a/Main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  int n, a, b;
+  cin >> n >> a >> b;
+  if ((b - a - 1) % 2 == 0) {
+    cout << "Borys" << endl;
+  } else {
+    cout << "Alice" << endl;
+  }
+}
