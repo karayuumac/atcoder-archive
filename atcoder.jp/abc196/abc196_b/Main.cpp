22494c2ba6174d258c9b97316af2fda3fbf76945
karayuu <karayan0614@gmail.com>
2021-03-20 21:03:49 +0900

    [AC] abc196 abc196_b

---
+++ atcoder.jp/abc196/abc196_b/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  string x;
+  cin >> x;
+
+  string s;
+
+  for (char c : x) {
+    if (c != '.') {
+      s += c;
+    } else {
+      break;
+    }
+  }
+  cout << s << endl;
+}
