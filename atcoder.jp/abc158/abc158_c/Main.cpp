36c128fba8d0b161f54c8dce0295e4455d03de15
karayuu <karayan0614@gmail.com>
2021-03-27 10:25:27 +0900

    [AC] abc158 abc158_c

---
+++ atcoder.jp/abc158/abc158_c/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  int a, b;
+  cin >> a >> b;
+
+  for (int i = 1; i <= 1250; i++) {
+    if (floor(i * 0.08) == a && floor(i * 0.1) == b) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+}
