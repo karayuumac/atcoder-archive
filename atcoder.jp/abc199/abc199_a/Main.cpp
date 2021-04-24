594fbf7cfc2e0c4c04ea168670bcc7b99d5d058b
karayuu <karayan0614@gmail.com>
2021-04-24 21:00:56 +0900

    [AC] abc199 abc199_a

---
+++ atcoder.jp/abc199/abc199_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define pi M_PI
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a * a + b * b < c * c) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
