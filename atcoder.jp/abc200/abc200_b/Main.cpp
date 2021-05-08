eb3ea2e349292996e78c6b45d03fceed51dddff1
karayuu <karayan0614@gmail.com>
2021-05-08 21:03:36 +0900

    [AC] abc200 abc200_b

---
+++ atcoder.jp/abc200/abc200_b/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector <T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  int n, k;
+  cin >> n >> k;
+
+  while (k > 0) {
+    if (n % 200 == 0) {
+      n /= 200;
+    } else {
+      n = n * 1000 + 200;
+    }
+    k--;
+  }
+  cout << n << endl;
+}
