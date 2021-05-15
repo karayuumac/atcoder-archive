a15d5642072832f3fcde0512c586073228d44b03
karayuu <karayan0614@gmail.com>
2021-05-15 21:02:00 +0900

    [AC] abc201 abc201_a

---
+++ atcoder.jp/abc201/abc201_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+
+#define rep(i, n) for(int i = 0; i < (int) n; i++)
+#define repd(i, n) for(int i = (int) (n - 1); i >= 0; i--)
+#define FOR(i, a, b) for(int i = a; i <= (int) b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int) b; i--)
+
+template<typename T>
+void load(int n, vector<T> &v) {
+  rep(i, n) {
+    T a;
+    cin >> a;
+    v.push_back(a);
+  }
+}
+
+signed main() {
+  vector<int> v;
+  load(3, v);
+  sort(v.begin(), v.end());
+  if (v[2] - v[1] == v[1] - v[0]) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
